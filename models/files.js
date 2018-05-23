437cb148bf641661ddbffc23b790de623843b138
CodeBrew28 <arshiam2@illinois.edu>
2018-05-23 10:38:31 -0700

    added teams'

--- models/files.js
+++ models/files.js
@@ -1,9 +1,9 @@
 const { Schema, SchemaTypes } = require('mongoose');
 
-
 const FilesSchema = new Schema({
     name: { type: String, required: true, unique: true },
+    url: { type: String, required: true, unique: true },
+    owner: { type: SchemaTypes.ObjectId, ref: 'Team' }
 })
   
-
 module.exports = m => m.model('Files', FilesSchema)
