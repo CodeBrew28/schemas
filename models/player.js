9b492d118f4c8caa580a027256e3a61e28b10707
CodeBrew28 <arshiam2@illinois.edu>
2018-05-26 09:40:22 -0700

    fixed player schema

--- models/player.js
+++ models/player.js
@@ -2,8 +2,8 @@ const { Schema, SchemaTypes } = require('mongoose');
 
 const PlayerSchema = new Schema({
     name: { type: String, required: true, unique: true },
-    url: { type: String, required: true, unique: true },
-    owner: { type: SchemaTypes.ObjectId, ref: 'Team' }
+    passqord: { type: String, required: true, unique: true },
+    team: { type: SchemaTypes.ObjectId, ref: 'Team' }
 })
   
 module.exports = m => m.model('Player', PlayerSchema)
